230e592d12ec2074961a78f14df71d2391817e29
curiousmind-squared <64873152+curiousmind-squared@users.noreply.github.com>
2022-03-07 00:10:18 -0300

    Add files via upload

---
+++ ZigProjects/valores/valores.zig
@@ -0,0 +1,19 @@
+const std = @import("std");
+const print = std.debug.print;
+
+pub fn main() void {
+    // inteiros
+    const one_plus_one: i32 = 1 + 1;
+    print("1 + 1 = {}\n", .{one_plus_one});
+
+    // floats
+    const seven_div_three: f32 = 7.0 / 3.0;
+    print("7.0 / 3.0 = {}\n", .{seven_div_three});
+
+    // boolean
+    print("{}\n{}\n{}\n", .{
+        true and false,
+        true or false,
+        !true,
+    });
+}
